4a1c695a180e20976cc1ceea58a428d9d4cc5578
vavilovm <vavilovmark@gmail.com>
2021-04-10 13:59:31 +0300

    fix api help

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -5,15 +5,15 @@ fun main() {
 
     val help =
         """API:
-h | help                        -- print help
-q | quit                        -- stop the program
-s | saveTo   FILENAMES          -- save current structure to given files
-l | loadFrom FILENAMES          -- add everything from given files (without removing current structure)
-v | values                      -- print all values
-n | nodesOf  VALUES             -- print all values from every given node
-create       VALUES             -- create nodes with given values. Does nothing if a node already exists
-add          value    VALUES    -- create nodes with all given values and adds everything to the first one's list
-clear                           -- delete all nodes
+* h | help                        -- print help
+* q | quit                        -- stop the program
+* s | saveTo   FILENAMES          -- save current structure to given files
+* l | loadFrom FILENAMES          -- add everything from given files (without removing current structure)
+* v | values                      -- print all values
+* n | nodesOf  VALUES             -- print all values from every given node
+* create       VALUES             -- create nodes with given values. Does nothing if a node already exists
+* add          value    VALUES    -- create nodes with all given values and adds everything to the first one's list
+* clear                           -- delete all nodes
 
 """
 
