8e68df4cd36973c14923d01f05e2f1e57cf80794
vavilovm <vavilovmark@gmail.com>
2021-04-10 13:57:01 +0300

    fix api help

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -3,19 +3,20 @@ import java.util.*
 fun main() {
     val input: Sequence<String> = generateSequence(::readLine)
 
-
     val help =
         """API:
-q | quit                        -- stop the program
 h | help                        -- print help
-create       VALUES             -- create nodes with given values. Does nothing if a node already exists
-add          value    VALUES    -- create nodes with all given values and adds everything to the first one's list
-clear                           -- delete all nodes
+q | quit                        -- stop the program
 s | saveTo   FILENAMES          -- save current structure to given files
 l | loadFrom FILENAMES          -- add everything from given files (without removing current structure)
 v | values                      -- print all values
 n | nodesOf  VALUES             -- print all values from every given node
+create       VALUES             -- create nodes with given values. Does nothing if a node already exists
+add          value    VALUES    -- create nodes with all given values and adds everything to the first one's list
+clear                           -- delete all nodes
+
 """
+
 
 
     val nodeManager = NodeManager()
